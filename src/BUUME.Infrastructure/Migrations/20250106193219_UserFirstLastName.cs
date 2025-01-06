3889a77c400ec784e05ec83f543dc9bf70ccb84c
ustuncem <ustuncem7@gmail.com>
2025-01-06 23:42:08 +0300

    feat(db_migrations): sync

---
+++ src/BUUME.Infrastructure/Migrations/20250106193219_UserFirstLastName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BUUME.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFirstLastName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "users",
+                newName: "last_name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "users",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "last_name",
+                table: "users",
+                newName: "name");
+        }
+    }
+}
