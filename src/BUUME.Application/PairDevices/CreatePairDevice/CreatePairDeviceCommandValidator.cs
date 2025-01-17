4c1d55945c645b843d0dee3e6cb4ef0f9d18a3f7
ahmetmertsakar <ahmetmert5070@gmail.com>
2025-01-17 17:25:59 +0300

    feat(application): add PairDeviceErrorCodes and update CreatePairDeviceCommandValidator

--- src/BUUME.Application/PairDevices/CreatePairDevice/CreatePairDeviceCommandValidator.cs
+++ src/BUUME.Application/PairDevices/CreatePairDevice/CreatePairDeviceCommandValidator.cs
@@ -6,10 +6,26 @@ internal sealed class CreatePairDeviceCommandValidator : AbstractValidator<Creat
 {
     public CreatePairDeviceCommandValidator()
     {
-        //RuleFor(x => x.Name)
-        //    .NotEmpty()
-        //    .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.MissingName)
-        //    .Matches("^[a-zA-ZÇçĞğİıÖöŞşÜü]+( [a-zA-ZÇçĞğİıÖöŞşÜü]+)*$")
-        //    .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.InvalidName);
+        RuleFor(x => x.DeviceName)
+            .NotEmpty()
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.MissingDeviceName)
+            .Matches("^[a-zA-ZÇçĞğİıÖöŞşÜü0-9]+( [a-zA-ZÇçĞğİıÖöŞşÜü0-9]+)*$")
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.InvalidDeviceName);
+
+        RuleFor(x => x.FcmToken)
+            .NotEmpty()
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.MissingFcmToken);
+
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.MissingUserId);
+
+        RuleFor(x => x.OperatingSystem)
+            .IsInEnum()
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.InvalidOperatingSystem);
+
+        RuleFor(x => x.IsActive)
+            .NotNull()
+            .WithErrorCode(PairDeviceErrorCodes.CreatePairDevice.MissingIsActive);
     }
 }
---
+++ src/BUUME.Application/PairDevices/PairDeviceErrorCodes.cs
@@ -0,0 +1,10 @@
+namespace BUUME.Application.PairDevices;
+
+public static class PairDeviceErrorCodes
+{
+    public const string MissingDeviceName = nameof(MissingDeviceName);
+    public const string MissingFcmToken = nameof(MissingFcmToken);
+    public const string MissingUserId = nameof(MissingUserId);
+    public const string InvalidOperatingSystem = nameof(InvalidOperatingSystem);
+    public const string MissingIsActive = nameof(MissingIsActive);
+}
